1d2b0c635d45dd63db0da4db8f0dac8bac9818a7
m-atalla <mo.atalla96@gmail.com>
2022-07-24 16:44:22 +0200

    change the custom impl for priority queue to `Node` enum

--- src/compression.rs
+++ src/compression.rs
@@ -6,6 +6,10 @@ pub enum Node {
     Leaf(Symbol),
 }
 
+pub trait Freq {
+    fn frequency(&self) -> u32;
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct Root {
     pub left: Box<Option<Node>>,
@@ -19,22 +23,31 @@ pub struct Symbol {
     pub frequency: u32,
 }
 
-macro_rules! impl_min_heap {
-    (for $($t:ty),+) => {
-        $(impl Ord for $t {
-            fn cmp(&self, other: &Self) -> Ordering {
-                other.frequency.cmp(&self.frequency)
+/// `BinaryHeap` implementation depends on `Ord` and `PartialOrd` traits 
+/// for managing how a value is pushed or popped from the internal data structure 
+/// this custom implementation flips the order effectively changing the `BinaryHeap`
+/// collect from a **max heap** (the default) to a **min heap** (priority queue)
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // TODO: maybe the following patterns should be replaced with a macro?
+        match self {
+            Node::Branch(node) => match other {
+                Node::Branch(other_node) =>  other_node.frequency.cmp(&node.frequency),
+                Node::Leaf(other_node) => other_node.frequency.cmp(&node.frequency),
+            },
+            Node::Leaf(node) => match other {
+                Node::Branch(other_node) => other_node.frequency.cmp(&node.frequency),
+                Node::Leaf(other_node) => other_node.frequency.cmp(&node.frequency)
             }
         }
-        impl PartialOrd for $t {
-            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-                Some(self.cmp(other))
-            }
-        })*
     }
 }
 
-impl_min_heap!(for Root, Symbol);
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 impl Root {
     pub fn new(frequency: u32) -> Root {
@@ -46,20 +59,42 @@ impl Root {
     }
 }
 
+impl Symbol {
+    pub fn new(frequency: u32, value: char) -> Symbol {
+        Symbol { value, frequency }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     use std::collections::BinaryHeap;
 
     #[test]
-    fn binary_heap_tinkering() {
+    fn priority_queue_impl_dequeue_order() {
         let mut priority = BinaryHeap::new();
 
-        priority.push(Root::new(30));
-        priority.push(Root::new(10));
-        priority.push(Root::new(20));
 
-        assert_eq!(priority.pop().unwrap().frequency, 10);
-        assert_eq!(priority.pop().unwrap().frequency, 20);
+        priority.push(
+            Node::Leaf(Symbol::new(20, 'a'))
+        );
+
+        priority.push(
+            Node::Branch(Root::new(10))
+        );
+
+
+        match priority.pop().unwrap() {
+            Node::Branch(node) => assert_eq!(node.frequency, 10),
+            _ => (), 
+        };
+
+        match priority.pop().unwrap() {
+            Node::Leaf(node) => {
+                assert_eq!(node.frequency, 20);
+                assert_eq!(node.value, 'a');
+            },
+            _ => (),
+        };
     }
 }
